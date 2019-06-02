428f95b189c69694ac7b28074d9bbdae01807b91
Joe Elliott <jelliott@qsrautomations.com>
2019-06-02 13:12:33 -0400

    Added some basic data types

--- src/datatypes.ts
+++ src/datatypes.ts
@@ -10,18 +10,31 @@ export class DataTypesScene extends Phaser.Scene {
     }
 
     init(params): void {
-    // TODO
+    
     }
 
     preload(): void {
-    // TODO
+    
     }
 
     create(): void {
+        /* boolean */
+        let addCircle : boolean   // declaration
+        addCircle = true          // assignment
 
+        /* number */
+        let radius : number = 20 // declaration and assignment
+
+        /* string */
+        let name : string = "joe elliott"
+
+        if(addCircle) {
+            this.add.circle(20, 20, radius, 0xee3322)
+            this.add.text(20, 20, name)
+        }
     }
     
     update(time): void {
-
+ 
     }
 };
