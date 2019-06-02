61cb4f017a30a82ab7a00ea92ff58310ffcdc47a
Joe Elliott <jelliott@qsrautomations.com>
2019-06-02 13:20:08 -0400

    Added arrays

--- src/datatypes.ts
+++ src/datatypes.ts
@@ -19,18 +19,26 @@ export class DataTypesScene extends Phaser.Scene {
 
     create(): void {
         /* boolean */
-        let addCircle : boolean   // declaration
-        addCircle = true          // assignment
+        let addCircle : boolean;   // declaration
+        addCircle = true;          // assignment
 
         /* number */
-        let radius : number = 20 // declaration and assignment
+        let radius : number = 20; // declaration and assignment
 
         /* string */
-        let name : string = "joe elliott"
+        let name : string = "joe elliott";
+
+        /* array */
+        let textToDraw : string[] = ["typescript", "game", "dev"];
 
         if(addCircle) {
-            this.add.circle(20, 20, radius, 0xee3322)
-            this.add.text(20, 20, name)
+            this.add.circle(20, 20, radius, 0xee3322);
+        }
+
+        this.add.text(300, 20, name);
+
+        for(let i = 0; i < textToDraw.length; i++) {
+            this.add.text(40 * i, 40 * i, textToDraw[i]);
         }
     }
     
